daa89bc1ab4cb79ba23912ff4971148dd0f35d53
Jordan Jaramillo <62086742+jordanrjdev@users.noreply.github.com>
2021-10-20 21:51:10 -0500

    feat : endpoint de subjects

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,24 @@
-import express from 'express';
-import AvatarRoutes from './routes/avatar.routes';
-import UserRoutes from './routes/users.routes';
-import path from 'path';
-import cors from 'cors';
+import express from "express";
+import AvatarRoutes from "./routes/avatar.routes";
+import UserRoutes from "./routes/users.routes";
+import SubjectRoutes from "./routes/subject.routes";
+import path from "path";
+import cors from "cors";
 
 const app: express.Application = express();
 const PORT = 4000;
 
 app.use(
   cors({
-    origin: '*',
+    origin: "*",
   })
 );
 app.use(express.json());
 app.use(AvatarRoutes);
+app.use(SubjectRoutes);
 app.use(UserRoutes);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`Express with Typescript! http://localhost:${PORT}`);
--- backend/src/routes/avatar.routes.ts
+++ backend/src/routes/avatar.routes.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-import { getAvatars, getAvatarbyId } from '../controllers/avatar.controller';
+import { Router } from "express";
+import { getAvatars, getAvatarbyId } from "../controllers/avatar.controller";
 
 const router: Router = Router();
 
-router.route('/avatar/:id').get(getAvatarbyId);
+router.route("/avatar/:id").get(getAvatarbyId);
 
-router.route('/avatars').get(getAvatars);
+router.route("/avatars").get(getAvatars);
 
 export default router;
